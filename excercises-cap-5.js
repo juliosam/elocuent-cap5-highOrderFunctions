484fe3f161a4c030f495aa7004f129526208350d
juliosam <ja.samaniegom@gmail.com>
2022-03-29 01:44:59 -0500

    2 excercises still 2 paragraphs

--- excercises-cap-5.js
+++ excercises-cap-5.js
@@ -3,3 +3,17 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
  let newa = arrays.reduce( (a,b) => a.concat(b))
  
  console.log(newa)
+
+
+
+ let loop = function(value,test,upDate,body){
+  
+  for(let count = 0 ; ; count++){
+     if (test(value)){
+            body(value);
+            value= upDate(value);
+     }              
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
