580faed20890859ef8d757c69a60ae2cfcb42c09
juliosam <ja.samaniegom@gmail.com>
2022-03-29 23:13:17 -0500

    4 excercise

--- excercises-cap-5.js
+++ excercises-cap-5.js
@@ -1,10 +1,22 @@
+const ej1 = "Ejercicio 1"
+const ej2 = "Ejercicio 2"
+const ej3 = "Ejercicio 3"
+const ej4 = "Ejercicio 4"
+
+
+const espacio =   " \n   \n"
+
+console.log(ej1); //-----------EJERCICIO 1 --------------------
+
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
  let newa = arrays.reduce( (a,b) => a.concat(b))
  
  console.log(newa)
 
+console.log(espacio)
 
+console.log(ej2); //-----------EJERCICIO 2 --------------------
 
  let loop = function(value,test,upDate,body){
   let varue = value;
@@ -18,6 +30,9 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 loop(3, n => n > 0, n => n-1, console.log);
 
 
+console.log(espacio)
+
+console.log(ej3); //-----------EJERCICIO 3 --------------------
 
 
 function every(array, test) {
@@ -45,3 +60,55 @@ function every(array, test) {
   console.log(allof([2, 4, 16], n => n < 10));
   // → false
   console.log(allof([], n => n < 10));
+
+
+  console.log(espacio)
+
+console.log(ej4);  //-----------EJERCICIO 4 --------------------
+  
+  
+  
+  function characterScript(code) {
+   for (let script of SCRIPTS) {
+     if (script.ranges.some(([from, to]) => {
+       return code >= from && code < to;
+     })) {
+       return script;
+     }
+   }
+   return null;
+ }
+
+
+ function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+
+  function dominantDirection(text) {
+   let scripts = countBy(text, char => {
+     let script = characterScript(char.codePointAt(0));
+     return script ? script.direction : "none";
+   }).filter(({direction}) => direction != "none");
+   let mayor = scripts.reduce((a,b) => {
+                 if (a.count > b.count){ return a.name}
+                     else{return b.name}
+   });
+   return mayor;
+   }
+ 
+ 
+ console.log(dominantDirection("Hello!"));
+ // → ltr
+ console.log(dominantDirection("Hey, مساء الخير"));
+ // → rtl
